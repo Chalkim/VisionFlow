67ad2ef77f990852a8b8f5966d34fdb1e1f9d075
Chalkim <jiangyi2012@outlook.com>
2025-03-04 05:33:28 +0800

    first commit

---
+++ src/main/java/chalkim/visionflow/VisionFlowApplication.java
@@ -0,0 +1,13 @@
+package chalkim.visionflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisionFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/chalkim/visionflow/VisionFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package chalkim.visionflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
